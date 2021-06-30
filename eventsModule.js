f5e9942aa3378cd5dad2755ac91523c357fbbc13
Jnmendza <jonatmendoza@gmail.com>
2021-06-30 15:36:58 -0700

    keys project beginnnig initial commit

---
+++ eventsModule.js
@@ -0,0 +1,27 @@
+var eventsModule = (function(dModule, uModule, cModule, wModule){
+    var addEventListeners = function(){
+
+        //character typing event listener
+
+        //click on download button event listener
+        
+    };
+
+                    
+    return {
+        //init function, initializes the test before start
+        init: function(duration, textNumber){
+            // fill the list of test words: data module
+            // fill the list of test words: UI module
+            // set the total test time
+            // update the time left: data Module
+            // update the time left: UI module
+            // move to a new word: data Module
+            // set active word: UI Module
+            // format the active word: UI module
+            // focus on text input: UI module
+            // add event listeners
+            addEventListeners();
+        }
+    };
+})(dataModule, UIModule, certificateModule, wordsModule);
